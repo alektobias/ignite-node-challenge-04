5968c1063f89c58faf5ecf56995f803f8f7af72f
Alek Tobias <alektobias@gmail.com>
2021-04-10 19:43:04 -0300

    challenge done

--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -1,3 +1,5 @@
+import { response } from "express";
+
 import { User } from "../../model/User";
 import { IUsersRepository } from "../../repositories/IUsersRepository";
 
@@ -9,7 +11,12 @@ class ListAllUsersUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User[] {
-    // Complete aqui
+    const user = this.usersRepository.findById(user_id);
+
+    if (!user) throw new Error("User not found");
+    if (!user.admin) throw new Error("You need to be admin");
+
+    return this.usersRepository.list();
   }
 }
 
